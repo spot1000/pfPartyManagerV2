b2a9dd38b7dff744ec72b9130e49f60545654bb7
Adam Lichter <adaml@pop-os.localdomain>
2018-08-20 21:34:37 -0230

    updated to what I had before the crash

--- client/src/components/CharacterSheet.tsx
+++ client/src/components/CharacterSheet.tsx
@@ -18,106 +18,113 @@ const styles = (theme: Theme) => createStyles({
     }
 });
 
-interface Props extends characterSheetState, WithStyles<typeof styles> {
+interface Props extends WithStyles<typeof styles> {
+    characterStats: characterSheetState
     onUpdateValue: (field:string, value:any) => void
     clearForm: () => void
 }
 
 class CharacterSheet extends React.Component<Props, characterSheetState>{
+    constructor(props: any){
+        super(props);
+    }
+
     onChange = (e:any) => {
         this.props.onUpdateValue(e.target.name, e.target.value)
     }
 
     render() {
-        const { classes, partyCode, playerName, characterName, characterClass, race, level, perceptionTotal, maxHP, clearForm} = this.props;
+        const classes = this.props.classes
+        const { partyCode, playerName, characterName, characterClass, race, level, perceptionTotal, maxHP} = this.props.characterStats;
         return (
             <div className='App'>
-            <div className={classes.container}>
-            <Input
-                placeholder="Party Code"
-                className={classes.input}
-                name="partyCode"
-                value={partyCode}
-                onChange={this.onChange}
-                inputProps={{
-                'aria-label': 'Description',
-                }}
-            />
-            <Input
-                placeholder="Player Name"
-                className={classes.input}
-                name='playerName'
-                value={playerName}
-                onChange={this.onChange}
-                inputProps={{
-                'aria-label': 'Description',
-                }}
-            />
-            <Input
-                placeholder="Character Name"
-                className={classes.input}
-                name='characterName'
-                value={characterName}
-                onChange={this.onChange}
-                inputProps={{
-                'aria-label': 'Description',
-                }}
-            />
-            <Input
-                placeholder="Class"
-                className={classes.input}
-                name='characterClass'
-                value={characterClass}
-                onChange={this.onChange}
-                inputProps={{
-                'aria-label': 'Description',
-                }}
-            />
-            <Input
-                placeholder="Race"
-                className={classes.input}
-                name='race'
-                value={race}
-                onChange={this.onChange}
-                inputProps={{
-                'aria-label': 'Description',
-                }}
-            />
-            <Input
-                placeholder="Level"
-                className={classes.input}
-                name='level'
-                value={level}
-                onChange={this.onChange}
-                inputProps={{
-                'aria-label': 'Description',
-                }}
-            />
-            <Input
-                placeholder="Perception Total"
-                className={classes.input}
-                name='perceptionTotal'
-                value={perceptionTotal}
-                onChange={this.onChange}
-                inputProps={{
-                'aria-label': 'Description',
-                }}
-            />
-            <Input
-                placeholder="Max HP"
-                className={classes.input}
-                name='maxHP'
-                value={maxHP}
-                onChange={this.onChange}
-                inputProps={{
-                'aria-label': 'Description',
-                }}
-            />
+                <div className="App" {...classes.container}>
+                    <Input
+                        placeholder="Party Code"
+                        className={classes.input}
+                        name="partyCode"
+                        value={partyCode}
+                        onChange={this.onChange}
+                        inputProps={{
+                        'aria-label': 'Description',
+                        }}
+                    />
+                    <Input
+                        placeholder="Player Name"
+                        className={classes.input}
+                        name='playerName'
+                        value={playerName}
+                        onChange={this.onChange}
+                        inputProps={{
+                        'aria-label': 'Description',
+                        }}
+                    />
+                    <Input
+                        placeholder="Character Name"
+                        className={classes.input}
+                        name='characterName'
+                        value={characterName}
+                        onChange={this.onChange}
+                        inputProps={{
+                        'aria-label': 'Description',
+                        }}
+                    />
+                    <Input
+                        placeholder="Class"
+                        className={classes.input}
+                        name='characterClass'
+                        value={characterClass}
+                        onChange={this.onChange}
+                        inputProps={{
+                        'aria-label': 'Description',
+                        }}
+                    />
+                    <Input
+                        placeholder="Race"
+                        className={classes.input}
+                        name='race'
+                        value={race}
+                        onChange={this.onChange}
+                        inputProps={{
+                        'aria-label': 'Description',
+                        }}
+                    />
+                    <Input
+                        placeholder="Level"
+                        className={classes.input}
+                        name='level'
+                        value={level}
+                        onChange={this.onChange}
+                        inputProps={{
+                        'aria-label': 'Description',
+                        }}
+                    />
+                    <Input
+                        placeholder="Perception Total"
+                        className={classes.input}
+                        name='perceptionTotal'
+                        value={perceptionTotal}
+                        onChange={this.onChange}
+                        inputProps={{
+                        'aria-label': 'Description',
+                        }}
+                    />
+                    <Input
+                        placeholder="Max HP"
+                        className={classes.input}
+                        name='maxHP'
+                        value={maxHP}
+                        onChange={this.onChange}
+                        inputProps={{
+                        'aria-label': 'Description',
+                        }}
+                    />
+                </div>
+                <div className="App">
+                    <Button variant="contained" color="primary">Save Character</Button>
+                    <Button variant="contained" onClick={this.props.clearForm}>Reset</Button>
+                </div>
             </div>
-            <Button variant="contained" color="primary">Save Character</Button>
-            <Button variant="contained" onClick={clearForm}>Reset</Button>
-
-        </div>
         )
     }
 }
--- client/src/containers/CharacterSheetContainer.tsx
+++ client/src/containers/CharacterSheetContainer.tsx
@@ -1,19 +1,29 @@
+import * as React from 'react'
 import CharacterSheet from '../components/CharacterSheet';
 import * as actions from '../actions/characterSheetActions';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { RootState } from '../reducers/rootReducer'
+import { characterSheetState } from '../types'
+
+interface Props {
+  characterStats : characterSheetState
+  onUpdateValue: (field:string, value:any) => void
+  clearForm: () => void
+}
 
 export function mapStateToProps(state: RootState) {
   return {
-    partyCode: state.characterSheet.partyCode,
-    playerName: state.characterSheet.playerName,
-    characterName: state.characterSheet.characterName,
-    characterClass: state.characterSheet.characterClass,
-    race: state.characterSheet.race,
-    level: state.characterSheet.level,
-    perceptionTotal:state.characterSheet.perceptionTotal,
-    maxHP: state.characterSheet.maxHP
+    characterStats: {
+      partyCode: state.characterSheet.partyCode,
+      playerName: state.characterSheet.playerName,
+      characterName: state.characterSheet.characterName,
+      characterClass: state.characterSheet.characterClass,
+      race: state.characterSheet.race,
+      level: state.characterSheet.level,
+      perceptionTotal:state.characterSheet.perceptionTotal,
+      maxHP: state.characterSheet.maxHP
+    }
   };
 }
 
@@ -24,4 +34,18 @@ export function mapDispatchToProps(dispatch: Dispatch<actions.characterSheetActi
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CharacterSheet);
+export class TestThing extends React.Component<Props, characterSheetState>{
+  render() {
+    return (
+      <div>
+        <CharacterSheet 
+          characterStats={this.props.characterStats} 
+          onUpdateValue={this.props.onUpdateValue}
+          clearForm={this.props.clearForm}
+        />
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TestThing);
